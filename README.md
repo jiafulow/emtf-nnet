13ed22db7c47b0f59f5bb4156557f27e0f4c5c0b
Jia Fu Low <jia.fu.low@cern.ch>
2021-03-10 16:06:18 -0500

    Add files

---
+++ README.md
@@ -0,0 +1,19 @@
+# emtf-nnet
+
+This package contains useful tools for EMTF neural net. It's used with Python 3 and TensorFlow 2.
+
+## Getting Started
+
+### Installation
+
+1. Install directly from source:
+
+``` bash
+git clone git@github.com:jiafulow/emtf-nnet.git
+cd emtf-nnet
+python -m pip install --ignore-installed -U -e .
+```
+
+## License
+
+This package is licensed under the terms of the Apache license. See [LICENSE](LICENSE) for more information.
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+install_requires = [
+    'numpy',
+    'six',
+]
+
+extras_require = {
+    'code-style': [
+        'flake8',
+        'isort',
+        'black',
+        'pre-commit',
+    ],
+    'tests': [
+        'pytest',
+    ],
+    'docs': [],
+}
+
+extras_require['complete'] = sorted({v for req in extras_require.values() for v in req})
+
+setuptools.setup(
+    install_requires=install_requires,
+    extras_require=extras_require,
+)
