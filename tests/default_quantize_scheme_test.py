59c163598a22436e253999979b4d27a84239b1c0
Jia Fu Low <jia.fu.low@cern.ch>
2021-04-10 23:28:28 -0400

    Update tests

--- tests/default_quantize_scheme_test.py
+++ tests/default_quantize_scheme_test.py
@@ -13,8 +13,7 @@ from emtf_nnet.keras.quantization.default_quantize_scheme import (
 
 
 def test_me():
-  input_shape = (40,)
-  nodes0, nodes1, nodes2, nodes_out = (24, 24, 16, 1)
+  nodes0, nodes1, nodes2, nodes_in, nodes_out = (24, 24, 16, 40, 1)
 
   custom_objects = dict(
       FeatureNormalization=FeatureNormalization,
@@ -24,18 +23,22 @@ def test_me():
   tf.keras.utils.get_custom_objects().update(custom_objects)
 
   model = tf.keras.Sequential()
-  model.add(tf.keras.layers.InputLayer(input_shape=input_shape, name='inputs'))
+  model.add(tf.keras.layers.InputLayer(input_shape=(nodes_in,), name='inputs'))
   model.add(FeatureNormalization(axis=-1, name='preprocessing'))
+  # Hidden layer 0
   model.add(MutatedDense(nodes0, kernel_initializer='glorot_uniform', use_bias=False, activation=None, name='dense'))
   model.add(MutatedBatchNormalization(momentum=0.99, epsilon=1e-4, name='batch_normalization'))
   model.add(TanhActivation(name='activation'))
+  # Hidden layer 1
   model.add(MutatedDense(nodes1, kernel_initializer='glorot_uniform', use_bias=False, activation=None, name='dense_1'))
   model.add(MutatedBatchNormalization(momentum=0.99, epsilon=1e-4, name='batch_normalization_1'))
   model.add(TanhActivation(name='activation_1'))
+  # Hidden layer 2
   model.add(MutatedDense(nodes2, kernel_initializer='glorot_uniform', use_bias=False, activation=None, name='dense_2'))
   model.add(MutatedBatchNormalization(momentum=0.99, epsilon=1e-4, name='batch_normalization_2'))
   model.add(TanhActivation(name='activation_2'))
-  model.add(tf.keras.layers.Lambda(lambda x: x / 64, name='lambda_normalization'))
+  # Output layer
+  model.add(tf.keras.layers.experimental.preprocessing.Rescaling(scale=1./64, name='rescaling'))
   model.add(MutatedDense(nodes_out, kernel_initializer='glorot_uniform', use_bias=False, activation=None, name='dense_final'))
   model.summary()
   assert model
