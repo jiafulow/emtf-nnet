2f0b15e8bd1e029995bfbe0f1fa6bf16bc693548
Jia Fu Low <jia.fu.low@cern.ch>
2021-09-17 10:12:24 -0500

    Remove slices/

--- emtf_nnet/slices/__init__.py
+++
@@ -1,5 +0,0 @@
-from .slices import (scatter_nd,
-                     gather,
-                     gather_nd,
-                     gather_indices_by_values,
-                     gather_inputs_by_outputs)
